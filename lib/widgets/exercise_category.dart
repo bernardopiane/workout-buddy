c47a73c8c36b4611ad4e6f8f7f2253c4d4fda3e1
Bernardo Piane <bernardopiane@users.noreply.github.com>
2024-08-09 12:47:58 -0300

    Modify: Refactored widget for better readability

--- lib/widgets/exercise_category.dart
+++ lib/widgets/exercise_category.dart
@@ -8,7 +8,6 @@ class ExerciseCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Display different icons based on the category
     return Row(
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.center,
@@ -16,33 +15,36 @@ class ExerciseCategory extends StatelessWidget {
         _getIcon(exercise.category),
         const SizedBox(width: 8.0),
         Text(
-          exercise.category ?? '',
-          style: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          exercise.category?.toUpperCase() ?? 'UNKNOWN',
+          style: const TextStyle(
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
     );
   }
 
-  _getIcon(String? category) {
-    // TODO get correct icons
-    if (category == "strength") {
-      return const Icon(Icons.work);
-    } else if (category == "stretching") {
-      return const Icon(Icons.vertical_split_outlined);
-    } else if (category == "plyometrics") {
-      return const Icon(Icons.sports_bar_outlined);
-    } else if (category == "strongman") {
-      return const Icon(Icons.add);
-    } else if (category == "powerlifting") {
-      return const Icon(Icons.ac_unit);
-    } else if (category == "cardio") {
-      return const Icon(Icons.fitness_center_outlined);
-    } else if (category == "olympic") {
-      return const Icon(Icons.access_alarm);
-    } else if (category == "weightlifting") {
-      return const Icon(Icons.access_time_filled_outlined);
-    } else {
-      return const Icon(Icons.circle_outlined);
+  static Icon _getIcon(String? category) {
+    switch (category?.toLowerCase()) {
+      case 'strength':
+        return const Icon(Icons.fitness_center);
+      case 'stretching':
+        return const Icon(Icons.self_improvement);
+      case 'plyometrics':
+        return const Icon(Icons.sports_kabaddi);
+      case 'strongman':
+        return const Icon(Icons.sports_handball);
+      case 'powerlifting':
+        return const Icon(Icons.power);
+      case 'cardio':
+        return const Icon(Icons.directions_run);
+      case 'olympic':
+        return const Icon(Icons.emoji_events);
+      case 'weightlifting':
+        return const Icon(Icons.sports_mma);
+      default:
+        return const Icon(Icons.circle_outlined);
     }
   }
 }
