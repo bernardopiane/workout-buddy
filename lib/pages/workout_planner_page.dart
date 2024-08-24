87ef52e366f0d44ef29f4c1eab9ae5aba1eda1c8
Bernardo Piane <bernardopiane@users.noreply.github.com>
2024-08-23 22:48:36 -0300

    Added: Complete button and selected days ordering

--- lib/pages/workout_planner_page.dart
+++ lib/pages/workout_planner_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:workout_buddy/global.dart';
 import 'package:workout_buddy/model/exercise.dart';
 import 'package:workout_buddy/model/workout_day.dart';
+import 'package:workout_buddy/pages/workout_page.dart';
 import 'package:workout_buddy/widgets/workout_selector.dart';
 
 class WorkoutPlannerPage extends StatefulWidget {
@@ -13,12 +14,7 @@ class WorkoutPlannerPage extends StatefulWidget {
 
 class _WorkoutPlannerPageState extends State<WorkoutPlannerPage>
     with TickerProviderStateMixin {
-  String? selectedDay;
-
   List<WorkoutDay> workoutDays = List.empty(growable: true);
-
-  WorkoutDay workoutDay = WorkoutDay(dayName: "", workouts: []);
-
   int _index = 0;
   TabController? _tabController;
 
@@ -36,89 +32,165 @@ class _WorkoutPlannerPageState extends State<WorkoutPlannerPage>
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Stepper(
-          currentStep: _index,
-          onStepCancel: () {
-            if (_index > 0) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Workout Planner'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Stepper(
+            type: StepperType.vertical,
+            currentStep: _index,
+            onStepTapped: (int index) {
               setState(() {
-                _index -= 1;
+                _index = index;
               });
-            }
-          },
-          onStepContinue: () {
-            if (_index <= 0) {
-              setState(() {
-                _index += 1;
-                _tabController =
-                    TabController(length: workoutDays.length, vsync: this);
-              });
-            }
-          },
-          onStepTapped: (int index) {
-            setState(() {
-              _index = index;
-            });
-          },
-          steps: <Step>[
-            Step(
-              title: const Text('Select Days'),
-              content: Wrap(
+            },
+            controlsBuilder: (BuildContext context, ControlsDetails details) {
+              return Row(
                 children: [
-                  ...dayOfWeek.map((e) {
+                  if (details.stepIndex > 0)
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_index > 0) {
+                          setState(() {
+                            _index -= 1;
+                          });
+                        }
+                      },
+                      child: const Row(
+                        children: [
+                          Icon(Icons.arrow_back_rounded),
+                          SizedBox(width: 8),
+                          Text("Previous"),
+                        ],
+                      ),
+                    ),
+                  const SizedBox(width: 8),
+                  if (details.stepIndex < 2)
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_index < 2) {
+                          setState(() {
+                            _index += 1;
+                          });
+                        }
+                      },
+                      child: const Row(
+                        children: [
+                          Text("Next"),
+                          SizedBox(width: 8),
+                          Icon(Icons.arrow_forward_rounded),
+                        ],
+                      ),
+                    ),
+                  // Last step, display a button to complete the workout
+                  if (details.stepIndex == 2)
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const WorkoutPage(),
+                        ));
+                      },
+                      child: const Row(
+                        children: [
+                          Icon(Icons.check),
+                          SizedBox(width: 8),
+                          Text("Complete"),
+                        ],
+                      ),
+                    ),
+                ],
+              );
+            },
+            steps: <Step>[
+              Step(
+                isActive: _index >= 0,
+                title: const Text('Select Days'),
+                content: Wrap(
+                  children: dayOfWeek.map((e) {
                     return CheckboxListTile(
                       title: Text(e),
                       value: workoutDays.any((element) => element.dayName == e),
                       onChanged: (bool? value) {
                         setState(() {
                           if (value == true) {
-                            workoutDays
-                                .add(WorkoutDay(dayName: e, workouts: []));
+                            workoutDays.add(WorkoutDay(dayName: e, workouts: []));
+                            // Reorder WorkoutDays to match the order of dayOfWeek
+                            List<String> dayOrder = dayOfWeek.toList();
+                            workoutDays.sort((a, b) {
+                              return dayOrder.indexOf(a.dayName).compareTo(dayOrder.indexOf(b.dayName));
+                            });
                           } else {
-                            workoutDays
-                                .removeWhere((element) => element.dayName == e);
+                            workoutDays.removeWhere((element) => element.dayName == e);
                           }
-                          _tabController = TabController(
-                              length: workoutDays.length, vsync: this);
+
+                          // Update the TabController to reflect the current number of tabs
+                          _tabController = TabController(length: workoutDays.length, vsync: this);
                         });
                       },
                     );
-                  }),
-                ],
+                  }).toList(),
+                ),
               ),
-            ),
-            Step(
-              title: const Text('Select Workouts'),
-              content: workoutDays.isEmpty
-                  ? const Center(child: Text('No days selected'))
-                  : Column(
-                      children: [
-                        TabBar(
-                          controller: _tabController,
-                          isScrollable: true,
-                          tabs: workoutDays
-                              .map((day) => Tab(text: day.dayName))
-                              .toList(),
-                        ),
-                        SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.5,
-                          child: TabBarView(
+              Step(
+                isActive: _index >= 1,
+                title: const Text('Select Workouts'),
+                content: workoutDays.isEmpty
+                    ? const Center(child: Text('No days selected'))
+                    : Column(
+                        children: [
+                          TabBar(
                             controller: _tabController,
-                            children: workoutDays.map((day) {
-                              return WorkoutSelector(
-                                workoutDay: day,
-                                addWorkout: _addWorkout,
-                                removeWorkout: _removeWorkout,
-                              );
-                            }).toList(),
+                            isScrollable: true,
+                            labelColor: Theme.of(context).colorScheme.primary,
+                            unselectedLabelColor:
+                                Theme.of(context).colorScheme.onSurface,
+                            indicatorColor:
+                                Theme.of(context).colorScheme.primary,
+                            tabs: workoutDays
+                                .map((day) => Tab(text: day.dayName))
+                                .toList(),
                           ),
-                        ),
-                      ],
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.5,
+                            child: TabBarView(
+                              controller: _tabController,
+                              children: workoutDays.map((day) {
+                                return WorkoutSelector(
+                                  workoutDay: day,
+                                  addWorkout: _addWorkout,
+                                  removeWorkout: _removeWorkout,
+                                );
+                              }).toList(),
+                            ),
+                          ),
+                        ],
+                      ),
+              ),
+              Step(
+                isActive: _index == 2,
+                title: const Text("Summary"),
+                state: _index == 2 ? StepState.complete : StepState.disabled,
+                content: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      "Summary",
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
                     ),
-            ),
-          ],
+                    const SizedBox(height: 16),
+                    Text("Total workout days: ${workoutDays.length}"),
+                    const SizedBox(height: 16),
+                    Text(
+                        "Total exercises: ${workoutDays.fold(0, (sum, day) => sum + day.workouts.length)}"),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -127,8 +199,7 @@ class _WorkoutPlannerPageState extends State<WorkoutPlannerPage>
   void _addWorkout(WorkoutDay workoutDay, Exercise exercise) {
     setState(() {
       workoutDays
-          .where((element) => element.dayName == workoutDay.dayName)
-          .first
+          .firstWhere((element) => element.dayName == workoutDay.dayName)
           .workouts
           .add(exercise);
     });
@@ -138,8 +209,7 @@ class _WorkoutPlannerPageState extends State<WorkoutPlannerPage>
   void _removeWorkout(WorkoutDay workoutDay, Exercise exercise) {
     setState(() {
       workoutDays
-          .where((element) => element.dayName == workoutDay.dayName)
-          .first
+          .firstWhere((element) => element.dayName == workoutDay.dayName)
           .workouts
           .remove(exercise);
     });
