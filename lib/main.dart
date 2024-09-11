5bdb349c410ef6706112ffabe0a2591b2a82352e
Bernardo Piane <bernardopiane@users.noreply.github.com>
2024-09-11 10:59:30 -0300

    Modify: Refactored to avoid using async functions in the main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
- // import 'package:supabase_flutter/supabase_flutter.dart';
+// import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:workout_buddy/model/favorites.dart';
 import 'package:workout_buddy/model/workout_plan.dart';
+import 'package:workout_buddy/model/workout_plan_manager.dart';
 import 'package:workout_buddy/pages/home_page.dart';
 import 'package:workout_buddy/theme_data.dart';
 
@@ -18,31 +19,41 @@ Future<void> main() async {
   //   anonKey:
   //       'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpZXVxcHlkc2poYXhtZmZxZm9jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE1MTQ1MjMsImV4cCI6MjAzNzA5MDUyM30.Y38cTModuek0kKNp5Qq8UsB3V073EmB75VahG6a_c0g',
   // );
+
+  final favorites = Favorites();
+  await favorites.loadFavorites();
+
+  final workoutPlan = WorkoutPlan();
+  await workoutPlan.loadFromSharedPreferences();
+
+  final userData = UserData();
+  await userData.loadUserData();
+
+  final workoutPlanManager = WorkoutPlanManager();
+  await workoutPlanManager.loadAllPlansFromSharedPreferences();
+  await workoutPlanManager.loadSelectedPlanFromSharedPreferences();
+
   runApp(
     MultiProvider(providers: [
-      ChangeNotifierProvider(create: (_) => Favorites()),
+      ChangeNotifierProvider(create: (_) => favorites),
       ChangeNotifierProvider(create: (_) => ExerciseList()),
-      ChangeNotifierProvider(create: (_) => WorkoutPlan()),
-      ChangeNotifierProvider(create: (_) => UserData()),
-    ], child: const MyApp()),
+      ChangeNotifierProvider(create: (_) => workoutPlan),
+      ChangeNotifierProvider(create: (_) => userData),
+      ChangeNotifierProvider(create: (_) => workoutPlanManager),
+    ], child: const WorkoutBuddyApp()),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class WorkoutBuddyApp extends StatelessWidget {
+  const WorkoutBuddyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    Provider.of<Favorites>(context, listen: false).loadFavorites();
-    Provider.of<WorkoutPlan>(context, listen: false)
-        .loadFromSharedPreferences();
-    Provider.of<UserData>(context, listen: false).loadUserData();
     return MaterialApp(
       title: 'Workout Buddy',
       theme: lightTheme,
       darkTheme: darkTheme,
-      themeMode: ThemeMode.system, // Use system theme by default
+      themeMode: ThemeMode.system,
       home: const HomePage(),
     );
   }
