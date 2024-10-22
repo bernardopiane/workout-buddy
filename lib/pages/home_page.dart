71103f2caa5af5a73e71d150f1f9a0deb68a1b33
Bernardo Piane <bernardopiane@users.noreply.github.com>
2024-10-22 14:50:31 -0300

    Add: upcoming workout display, new styles, exercise list page link

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:workout_buddy/model/exercise_filters.dart';
 import 'package:workout_buddy/model/workout_plan_manager.dart';
+import 'package:workout_buddy/pages/exercise_list_page.dart';
 import 'package:workout_buddy/pages/onboarding_page.dart';
 import 'package:workout_buddy/pages/user_page.dart';
 import 'package:workout_buddy/pages/workout_details_page.dart';
 import 'package:workout_buddy/pages/workout_plan_manager_page.dart';
 import 'package:workout_buddy/pages/workout_planner_page.dart';
 import '../model/user_data.dart';
+import '../model/workout_day.dart';
 import '../utils.dart';
 import 'main_page.dart';
 
@@ -35,7 +38,8 @@ class HomePage extends StatelessWidget {
               ],
             ),
             const SizedBox(height: 16.0),
-            _buildNextWorkoutsSection(context),
+            // _buildNextWorkoutsSection(context),
+            _buildUpcomingWorkoutsSection(context),
             const SizedBox(height: 16.0),
             _buildTodayWorkoutsSection(context),
             const SizedBox(height: 16.0),
@@ -64,17 +68,21 @@ class HomePage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('Stats', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        const Text('Stats',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
         const SizedBox(height: 16),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Selector<WorkoutPlanManager, int>(
-              selector: (_, manager) => manager.selectedPlan?.workoutDays
-                  .map((day) => day.workouts.length)
-                  .reduce((a, b) => a + b) ?? 0,
+              selector: (_, manager) =>
+                  manager.selectedPlan?.workoutDays
+                      .map((day) => day.workouts.length)
+                      .reduce((a, b) => a + b) ??
+                  0,
               builder: (context, totalWorkouts, child) {
-                return _buildStatCard(title: 'Total Workouts', value: '$totalWorkouts');
+                return _buildStatCard(
+                    title: 'Total Workouts', value: '$totalWorkouts');
               },
             ),
             const SizedBox(width: 16),
@@ -98,7 +106,8 @@ class HomePage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("Next Workouts", style: Theme.of(context).textTheme.headlineSmall),
+        Text("Upcoming Workouts",
+            style: Theme.of(context).textTheme.headlineSmall),
         const SizedBox(height: 16.0),
         Consumer<WorkoutPlanManager>(
           builder: (context, workoutPlanManager, child) {
@@ -109,8 +118,9 @@ class HomePage extends StatelessWidget {
             var today = DateTime.now();
             var todayString = convertNumberToWeekday(today.weekday);
 
-            var nextWorkoutDay = workoutPlanManager.selectedPlan!.workoutDays.firstWhere(
-                  (day) => day.dayName == todayString || day.workouts.isNotEmpty,
+            var nextWorkoutDay =
+                workoutPlanManager.selectedPlan!.workoutDays.firstWhere(
+              (day) => day.dayName == todayString || day.workouts.isNotEmpty,
               orElse: () => workoutPlanManager.selectedPlan!.workoutDays.first,
             );
 
@@ -118,9 +128,12 @@ class HomePage extends StatelessWidget {
               return const Text("No workouts planned for today");
             }
 
-            var muscles = nextWorkoutDay.workouts.map((workout) => workout.images![0]).toSet().toList();
-
-            return SizedBox(height: 128, child: MuscleOverlay(muscles));
+            var muscles = nextWorkoutDay.workouts
+                .map((workout) => workout.images![0])
+                .toSet()
+                .toList();
+            return Container();
+            // return SizedBox(height: 256, child: MuscleOverlay(muscles));
           },
         ),
       ],
@@ -136,7 +149,8 @@ class HomePage extends StatelessWidget {
           child: Placeholder(), // Replace with your icon
         ),
         Text(title),
-        Text(value, style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        Text(value,
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
       ],
     );
   }
@@ -150,19 +164,22 @@ class HomePage extends StatelessWidget {
           builder: (context, userData, child) {
             return userData.name.isNotEmpty
                 ? Row(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(userData.name, style: const TextStyle(fontWeight: FontWeight.bold)),
-                    const SizedBox(height: 8),
-                    Text("${userData.age} yrs", style: Theme.of(context).textTheme.bodyMedium),
-                  ],
-                ),
-                const SizedBox(width: 16),
-                const Placeholder(fallbackHeight: 50, fallbackWidth: 50),
-              ],
-            )
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(userData.name,
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold)),
+                          const SizedBox(height: 8),
+                          Text("${userData.age} yrs",
+                              style: Theme.of(context).textTheme.bodyMedium),
+                        ],
+                      ),
+                      const SizedBox(width: 16),
+                      const Placeholder(fallbackHeight: 50, fallbackWidth: 50),
+                    ],
+                  )
                 : const Text("Please complete your profile");
           },
         ),
@@ -174,7 +191,8 @@ class HomePage extends StatelessWidget {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        const Text('Today\'s Workouts', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        const Text('Today\'s Workouts',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
         const SizedBox(height: 16.0),
         Consumer<WorkoutPlanManager>(
           builder: (context, workoutPlanManager, child) {
@@ -199,7 +217,8 @@ class HomePage extends StatelessWidget {
                   onTap: () => Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => WorkoutDetailsPage(workoutPlan: workoutPlanManager.selectedPlan!),
+                      builder: (context) => WorkoutDetailsPage(
+                          workoutPlan: workoutPlanManager.selectedPlan!),
                     ),
                   ),
                   child: Card(
@@ -218,7 +237,8 @@ class HomePage extends StatelessWidget {
           onPressed: () {
             Navigator.push(
               context,
-              MaterialPageRoute(builder: (context) => const WorkoutPlannerPage()),
+              MaterialPageRoute(
+                  builder: (context) => const WorkoutPlannerPage()),
             );
           },
           child: const Text('Create New Workout Plan'),
@@ -232,7 +252,8 @@ class HomePage extends StatelessWidget {
       children: [
         const Text('Calories burned so far'),
         const SizedBox(height: 16.0),
-        Text(caloriesBurned.toStringAsFixed(2), style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
+        Text(caloriesBurned.toStringAsFixed(2),
+            style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold)),
       ],
     );
   }
@@ -243,12 +264,19 @@ class HomePage extends StatelessWidget {
       {'text': 'User Page', 'page': const UserPage()},
       {'text': 'Workout Plan Manager', 'page': const WorkoutPlanManagerPage()},
       {'text': 'Onboarding', 'page': const OnboardingPage()},
+      {
+        'text': 'Exercise List Page',
+        'page': ExerciseListPage(),
+      },
     ];
 
     return Column(
       children: buttons.map((button) {
         return ElevatedButton(
-          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => button['page'] as Widget)),
+          onPressed: () => Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => button['page'] as Widget)),
           child: Text(button['text'] as String),
         );
       }).toList(),
@@ -266,49 +294,120 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+  _buildUpcomingWorkoutsSection(BuildContext context) {
+    const workoutToDisplay = 2;
+
+    // Gets the next X workouts days from the workout plan manager based on the current date
+    var today = DateTime.now();
+    var todayString = convertNumberToWeekday(today.weekday);
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text("Upcoming Workouts",
+            style: Theme.of(context).textTheme.headlineSmall),
+        const SizedBox(height: 16.0),
+
+        // Use Consumer to access the WorkoutPlanManager data
+        Consumer<WorkoutPlanManager>(
+          builder: (context, workoutPlanManager, child) {
+            if (workoutPlanManager.selectedPlan == null ||
+                workoutPlanManager.selectedPlan!.workoutDays.isEmpty) {
+              return const Text("No upcoming workouts.");
+            }
+
+            // Find the index of today in the workoutDays list
+            List<WorkoutDay> allWorkoutDays =
+                workoutPlanManager.selectedPlan!.workoutDays;
+
+            // Get upcoming workouts starting from today
+            List<WorkoutDay> upcomingWorkouts = [];
+            int currentIndex =
+                allWorkoutDays.indexWhere((day) => day.dayName == todayString);
+
+            // Collect the next 2 upcoming workout days (wrap around if necessary)
+            for (int i = 0; i < workoutToDisplay; i++) {
+              int nextIndex = (currentIndex + i) % allWorkoutDays.length;
+              upcomingWorkouts.add(allWorkoutDays[nextIndex]);
+            }
+
+            return Row(
+                children: upcomingWorkouts.map((workoutDay) {
+              // Get the primary muscles being worked on for this workout day
+              Set<String> primaryMuscles = {};
+
+              for (var workout in workoutDay.workouts) {
+                primaryMuscles.add(workout.primaryMuscles!.first);
+              }
+
+              return Expanded(
+                child: Card(
+                  elevation: 2,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(workoutDay.dayName),
+                        const SizedBox(height: 8),
+                        Text(primaryMuscles.join(', ').toUpperCase(),
+                            style: const TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.bold)),
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            }).toList());
+          },
+        ),
+      ],
+    );
+  }
 }
+
+/*
 
 class MuscleOverlay extends StatelessWidget {
   final List<String> images;
 
   const MuscleOverlay(this.images, {super.key});
 
+  // TODO Fix image scaling
+
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(50),
-      child: SizedBox(
-        height: 256,
-        width: MediaQuery.of(context).size.width,
-        child: Stack(
-          children: [
-            Positioned.fill(
-              child: ClipPath(
-                clipper: DiagonalClipper(isLeft: true),
-                child: Image.asset(
-                  "lib/data/${images[0]}",
-                  fit: BoxFit.cover,
-                  alignment: Alignment.centerLeft,
-                ),
+      child: Stack(
+        children: [
+          Positioned.fill(
+            child: ClipPath(
+              clipper: DiagonalClipper(isLeft: true),
+              child: Image.asset(
+                "lib/data/${images[0]}",
+                fit: BoxFit.cover,
+                alignment: Alignment.centerLeft,
               ),
             ),
-            Positioned.fill(
-              child: ClipPath(
-                clipper: DiagonalClipper(isLeft: false),
-                child: Image.asset(
-                  "lib/data/${images[1]}",
-                  fit: BoxFit.cover,
-                  alignment: Alignment.centerRight,
-                ),
+          ),
+          Positioned.fill(
+            child: ClipPath(
+              clipper: DiagonalClipper(isLeft: false),
+              child: Image.asset(
+                "lib/data/${images[1]}",
+                fit: BoxFit.cover,
+                alignment: Alignment.centerRight,
               ),
             ),
-            Positioned.fill(
-              child: CustomPaint(
-                painter: DiagonalSeparatorPainter(),
-              ),
+          ),
+          Positioned.fill(
+            child: CustomPaint(
+              painter: DiagonalSeparatorPainter(),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -364,3 +463,4 @@ class DiagonalSeparatorPainter extends CustomPainter {
     return false;
   }
 }
+*/
