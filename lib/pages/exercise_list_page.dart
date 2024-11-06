5c508a8383ddfe83b175677fbf17356b48e913af
Bernardo Piane <bernardopiane@users.noreply.github.com>
2024-11-06 15:35:09 -0300

    Add: animation to search toggle

--- lib/pages/exercise_list_page.dart
+++ lib/pages/exercise_list_page.dart
@@ -31,25 +31,52 @@ class _ExerciseListPageState extends State<ExerciseListPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: !_isSearching
-            ? const Text("Exercise List Page")
-            : TextField(
-                controller: _searchController,
-                autofocus: true,
-                decoration: const InputDecoration(
-                  hintText: "Search exercises...",
-                  border: InputBorder.none,
-                ),
-                onChanged: (query) {
-                  setState(() {
-                    _searchText = query;
-                  });
-                },
+        title: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 300),
+          child: _isSearching
+              ? TextField(
+                  key: const ValueKey("searchField"),
+                  controller: _searchController,
+                  autofocus: true,
+                  style: Theme.of(context).textTheme.titleLarge,
+                  decoration: InputDecoration(
+                    hintText: "Search exercises...",
+                    hintStyle: Theme.of(context).textTheme.titleLarge?.copyWith(
+                          color: Theme.of(context)
+                              .colorScheme
+                              .onSurface
+                              .withOpacity(0.6),
+                        ),
+                    border: InputBorder.none,
+                  ),
+                  onChanged: (query) {
+                    setState(() {
+                      _searchText = query;
+                    });
+                  },
+                )
+              : Align(
+                // Fixes the title text alignment jumping to the center
+                alignment: Alignment.centerLeft,
+                child: Text(
+                    "Exercise List Page",
+                    key: const ValueKey("pageTitle"),
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
               ),
+        ),
         actions: [
-          IconButton(
-            icon: Icon(_isSearching ? Icons.close : Icons.search),
-            onPressed: _toggleSearch,
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 300),
+            transitionBuilder: (child, animation) => ScaleTransition(
+              scale: animation,
+              child: child,
+            ),
+            child: IconButton(
+              key: ValueKey(_isSearching ? "closeIcon" : "searchIcon"),
+              icon: Icon(_isSearching ? Icons.close : Icons.search),
+              onPressed: _toggleSearch,
+            ),
           ),
         ],
       ),
