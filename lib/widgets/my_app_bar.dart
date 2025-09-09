a877775202d7c86c730ea965344460909d773777
Bernardo Piane <bernardopiane@users.noreply.github.com>
2025-09-09 16:27:10 -0300

    Added MyAppBar widget
    This custom app bar includes a title and a settings icon that navigates to the SettingsPage.

---
+++ lib/widgets/my_app_bar.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../pages/settings_page.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text(title),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          onPressed: () {
+            //   Navigates to Settings Page
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const SettingsPage(),
+              ),
+            );
+          },
+          icon: const Icon(Icons.settings),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
